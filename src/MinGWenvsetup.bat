91acfe2c88206c75d487169cb163b2f0857b94b7
Lake Hu <lake.hu@commscope.com>
2020-10-22 17:29:58 +0800

    Please set mingw32/bin into PATH before vscode is launched from command line(PATH includes mingw32/bin

--- src/MinGWenvsetup.bat
+++
@@ -1,5 +0,0 @@
-PATH=%PATH%;C:\MinGW\gdb32\bin;C:\MinGW\mingw32\bin;C:\MinGW\usr\bin;C:\MinGW\mingw32\bin
-set COMPILER_PATH="C:\MinGW\usr\lib\gcc\i686-pc-msys\7.3.0;C:\MinGW\usr\lib\gcc\i686-pc-msys;C:\MinGW\usr\lib\gcc"
-set LIBRARY_PATH="C:\MinGW\usr\lib\gcc\i686-pc-msys\7.3.0;C:\MinGW\usr\lib\gcc\i686-pc-msys\7.3.0\..\..\..\..\i686-pc-msys\lib;C:\MinGW\usr\lib;C:\MinGW\usr\lib\w32api"
-
-C:\MinGW\usr\bin\gcc -Wall -ggdb hello.c -o a.exe
